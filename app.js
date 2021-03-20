4a8265ee0f35b1d85dc6c81764c83c429094de07
sanghyeonchoi <76832243+sanghyeonchoi@users.noreply.github.com>
2021-03-20 13:32:55 +0900

    Routing

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
 const app = express();
 
 const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("Hello from my ass");
 const handleProfile = (req, res) => res.send("You are on my profile");
@@ -22,4 +20,5 @@ app.use(morgan("dev"));
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
-app.listen(PORT, handleListening);
+
+export default app;
